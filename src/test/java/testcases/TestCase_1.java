48c430c8ecdaaa3d5c2fde70ae81707b730e72f9
kavita00007 <kavitarpatil27@gmail.com>
2020-08-26 14:32:06 +0530

    first case code added

--- src/test/java/testcases/TestCase_1.java
+++ src/test/java/testcases/TestCase_1.java
@@ -1,5 +1,41 @@
 package testcases;
 
-public class TestCase_1 {
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestCase_1 {
+	
+	WebDriver driver;
+	String url= "https://traveltriangle.com/";
+	
+	
+	@Before
+	public void browser_setup() {
+		
+		driver= new FirefoxDriver();
+		driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+	}
+	
+	@Test
+	public void Check_website() {
+		
+		driver.get(url);
+        String expected = "Personalized Holiday Packages | Customized Tour Packages from multiple local and trusted travel agents";
+        String actual =driver.getTitle();
+        Assert.assertEquals("Page validation",expected,actual);
+		
+	}
+     
+	@After
+	public void clean_up() {
+		driver.quit();
+	}
 }
